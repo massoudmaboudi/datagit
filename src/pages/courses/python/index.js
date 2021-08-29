fc8cc35ad95bade5c79cb60053aa59548a11b074
Massoud Maboudi <massoud.maboudi@gmail.com>
2021-08-30 00:20:18 +0300

    a bit of change

--- src/pages/courses/python/index.js
+++ src/pages/courses/python/index.js
@@ -60,32 +60,32 @@ function CourseInner() {
             description="وبسایت دیتاگیت برای این راه اندازی شده که شاید کمک کوچکی باشه برای کسانی که دوست دارند برنامه نویسی رو یاد بگیرن ولی همیشه فکر میکنن که کار سختیه! توی این وبسایت به هم کمک میکنیم تا مسیر یادگیری رو برای همدیگه آسان تر کنیم."
             keywords={['مسعود معبودی', 'Massoud Maboudi', 'دیتاگیت', 'datagit', 'datagit.ir', 'مسعود معبودی رزومه', 'رزومه مسعود معبودی', 'Massoud Maboudi profile', 'دیتا گیت', 'مسعود معبودی دیتا گیت', 'آموزش پایتون', 'آموزش رایگان پایتون']}
         >
-                <header className={styles.courseHeader}>
-                    <div className="container">
-                        <div className="row">
-                            <div className={clsx(styles.course_logo_div, "col col--4")}>
-                                <img className={styles.headerImg} src="/img/python/python-logo.svg" />
+            <header className={styles.courseHeader}>
+                <div className="container">
+                    <div className="row">
+                        <div className={clsx(styles.course_logo_div, "col col--4")}>
+                            <img className={styles.headerImg} src="/img/python/python-logo.svg" />
 
-                            </div>
-                            <div className="col col--6">
-                                <img className={styles.headerImgMobile} src="/img/python/python-logo.svg" />
-                                <div>
-                                    <h2 className={styles.course__title}>آموزش زبان برنامه نویسی پایتون</h2>
-                                    <p className={styles.course__subtitle}>با توجه به اینکه پایتون در حال حاضر یکی از بهترین زبان های برنامه نویسی دنیا هستش، اگر الان نخواید یاد بگیریدش، پس کِی میخواید برید سراغش؟</p>
-                                    <p className={styles.course__subtitle}>آموزش ها و مثال های کاربردی دیتاگیت به شما کمک میکنه تا قدم به قدم خودتون رو توی پایتون قوی تر کنید.  </p>
-                                    {/* <div className="search"> */}
-                                    {/* <input
+                        </div>
+                        <div className="col col--6">
+                            <img className={styles.headerImgMobile} src="/img/python/python-logo.svg" />
+                            <div>
+                                <h2 className={styles.course__title}>آموزش زبان برنامه نویسی پایتون</h2>
+                                <p className={styles.course__subtitle}>با توجه به اینکه پایتون در حال حاضر یکی از بهترین زبان های برنامه نویسی دنیا هستش، اگر الان نخواید یاد بگیریدش، پس کِی میخواید برید سراغش؟</p>
+                                <p className={styles.course__subtitle}>آموزش ها و مثال های کاربردی دیتاگیت به شما کمک میکنه تا قدم به قدم خودتون رو توی پایتون قوی تر کنید.  </p>
+                                {/* <div className="search"> */}
+                                {/* <input
                                     className={classnames("shadow--lw", styles.cookbookSearch)}
                                     type="text"
                                     onChange={(event) => setSearchTerm(event.currentTarget.value)}
                                     placeholder="🔍 Search..." /> */}
-                                    {/* </div> */}
-                                </div>
+                                {/* </div> */}
                             </div>
                         </div>
                     </div>
-                </header >
-                {/* <div className="container"> */}
+                </div>
+            </header >
+            <div className="container">
                 <div class={styles.card_item_container}>
                     <div class="row">
                         {courseItems.map((props, idx) => (
@@ -93,6 +93,7 @@ function CourseInner() {
                         ))}
                     </div>
                 </div>
+            </div>
         </Layout >
     );
 }
