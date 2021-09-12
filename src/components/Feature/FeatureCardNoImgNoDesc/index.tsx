1c7da28f063b5242bbc63f151a92b322fcab0741
Massoud Maboudi <massoud.maboudi@gmail.com>
2021-09-12 23:02:29 +0300

    a small change

--- src/components/Feature/FeatureCardNoImgNoDesc/index.tsx
+++ src/components/Feature/FeatureCardNoImgNoDesc/index.tsx
@@ -16,8 +16,8 @@ const FeatureCardNoImgNoDesc = memo(function ({ course }: { course: any }) {
               <div className={styles.titleIconsRow}>
                 <div className={clsx(styles.counter)}>{course.id}</div>
                 <div className={styles.titleIconsRowTitle}>
-                  {/* <div className="avatar__name">{course.title}</div>*/
-                    <p className={clsx(styles.titleIconsRowSubTitle, "avatar__subtitle")}>{course.title}</p>}
+                  <div className="avatar__name">{course.title}</div>
+                  {/* <p className={clsx(styles.titleIconsRowSubTitle, "avatar__subtitle")}>{course.title}</p> */}
                 </div>
                 {/* <div className={styles.titleIconsRowIcons}>
                   <FeatureTagIcons tags={course.tags} />
